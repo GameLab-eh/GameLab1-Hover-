bca2641d40762f9d7a6fb1aa04c0e942b1e221f3
andrea-cantatore <andreacantatore01@gmail.com>
2024-01-13 15:53:21 +0100

    some power ups
    added invisibility, wall and jump

--- Assets/Scripts/PlayerScripts/PlayerController.cs
+++ Assets/Scripts/PlayerScripts/PlayerController.cs
@@ -1,23 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting.Dependencies.Sqlite;
 using UnityEngine;
+
+
 
 public class PlayerController : MonoBehaviour
 {
+    //for designer
+    [Header("Input")]
+    [SerializeField] KeyCode keyJump1 = KeyCode.J;
+    [SerializeField] KeyCode keyJump2 = KeyCode.Alpha1;
+    [SerializeField] KeyCode keyWall1 = KeyCode.K;
+    [SerializeField] KeyCode keyWall2 = KeyCode.Alpha2;
+    [SerializeField] KeyCode keyInvisibility1 = KeyCode.L;
+    [SerializeField] KeyCode keyInvisibility2 = KeyCode.Alpha3;
+
+    //checks
+    private bool _isAbleToMove = true;
+    private bool _isAlive = true;
+    private bool _isGrounded = true;
+    private bool _isInvisible = false;
+
     //general variables
-    [SerializeField] private Rigidbody rb;
-    private bool isPlayerAbleToMove = true;
     private int _playerScore;
 
     //basic movement variables
     private float _horizontal;
     private float _vertical;
-    [SerializeField] float movementSpeed;
+    [Header("Physics")]
+    [SerializeField, Min (0)] float movementSpeed;
     [SerializeField] float maxSpeed;
     [SerializeField] float rotationSpeed;
     private float _normalMaxSpeed; //used for store MaxSpeedVariable to restore to default when changed (currently not used)
 
-    public Rigidbody Rb { get => rb; private set => rb = value; }
+    //for buff and malus variables
+    //jump
+    [SerializeField] float jumpPower;
+    [SerializeField] float _jumpStack = 10;
+    [SerializeField] float _invisibilityStack = 10;
+    
+
+
+    [field:SerializeField]public Rigidbody Rb { get; private set; }
 
     private void Awake()
     {
@@ -26,18 +51,42 @@ public class PlayerController : MonoBehaviour
 
     private void Update()
     {
-        if (isPlayerAbleToMove)
+        if (_isAlive)
         {
-            _horizontal = Input.GetAxisRaw("Horizontal");
-            _vertical = Input.GetAxisRaw("Vertical");
-            rotate();
+            if (_isAbleToMove)
+            {
+                _horizontal = Input.GetAxisRaw("Rotation");
+                _vertical = Input.GetAxisRaw("Move");
+                rotate();
+            }
+            if (Input.GetKeyDown(keyJump1) || Input.GetKeyDown(keyJump2))
+            {
+                Jump();
+            }
+            if (Input.GetKeyDown(keyWall1) || Input.GetKeyDown(keyWall2))
+            {
+                Wall();
+            }
+            if (Input.GetKeyDown(keyInvisibility1) || Input.GetKeyDown(keyInvisibility2)) 
+            {
+                Debug.Log(_isInvisible);
+                StartCoroutine(Invisibility());
+            }
         }
+       
     }
 
 
     void FixedUpdate()
     {
         Move();
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Ground")
+        {
+            _isGrounded = true;
+        }
     }
 
     private void rotate()
@@ -48,10 +97,38 @@ public class PlayerController : MonoBehaviour
 
     private void Move()
     {
-        if (rb.velocity.magnitude < maxSpeed)
+        if (Rb.velocity.magnitude < maxSpeed)
         {
             Vector3 move = transform.forward * _vertical * movementSpeed;
             Rb.AddForce(move, ForceMode.Force);
+        }
+    }
+    
+    private void Jump()
+    {
+        if(_isGrounded && _jumpStack > 0)
+        {
+            Rb.AddForce(transform.up * jumpPower, ForceMode.Impulse);
+            _jumpStack--;
+            _isGrounded = false;
+        }
+    }
+
+    private void Wall()
+    {
+        Debug.Log("ENTRA?!?!?!?");
+    }
+
+    public IEnumerator Invisibility()
+    {
+        if(!_isInvisible)
+        {
+            _invisibilityStack--;
+            _isInvisible = true;
+            Debug.Log("hey sono invisibile");
+            yield return new WaitForSeconds(5f);
+            _isInvisible = false;
+            Debug.Log("hey non sono più invisibile");
         }
     }
 }
