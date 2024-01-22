ecf46b3734f70c6e777bcd1f89f2fccb511803f0
andrea-cantatore <andreacantatore01@gmail.com>
2024-01-22 22:50:51 +0100

    ia spawn point

---
+++ Assets/Scripts/ai/Ai Pooler.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+public class AiPooler : MonoBehaviour
+{
+    [Header("Spawn Settings")] [SerializeField, Tooltip("insert enemys")]
+    private GameObject[] _enemyPrefab;
+
+    [SerializeField] private List<Transform> _spawnPointList;
+
+    [SerializeField, Tooltip("Number of spawns for each enemy")]
+    private int _spawnCounter;
+
+    private void Awake()
+    {
+        if ((_enemyPrefab.Length * _spawnCounter) > _spawnPointList.Count)
+        {
+            return;
+        }
+
+        foreach (GameObject enemy in _enemyPrefab)
+        {
+            for (int i = 0; i < _spawnCounter; i++)
+            {
+                int indexNumber = Random.Range(0, _spawnPointList.Count);
+                Instantiate(enemy, _spawnPointList[index: indexNumber]);
+                _spawnPointList.RemoveAt(indexNumber);
+            }
+        }
+    }
+}
