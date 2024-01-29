71f0d445acfa69d9ee299798cb534ddc3c248136
STRANOstudios <friand2021@gmail.com>
2024-01-29 01:16:41 +0100

    Update PowerUps

---
+++ Assets/_Scripts/Graphic/AnimationRotation.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationRotation : MonoBehaviour
+{
+
+    private float rotationSpeed = 30f;
+    private float oscillationSpeed = 1f;
+    private float oscillationHeight = 0.1f; 
+
+    private float initialY;
+
+    void Start()
+    {
+        initialY = transform.position.y;
+    }
+
+    void Update()
+    {
+        transform.Rotate(Vector3.up, rotationSpeed * Time.deltaTime);
+
+        float oscillation = Mathf.Sin(Time.time * oscillationSpeed) * oscillationHeight;
+        transform.position = new Vector3(transform.position.x, initialY + oscillation, transform.position.z);
+    }
+}
